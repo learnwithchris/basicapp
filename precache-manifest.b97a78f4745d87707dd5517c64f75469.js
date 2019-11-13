a10e89fa2131ceed267e7d3d8de08ed349a70c67
learnwithchris <56309725+learnwithchris@users.noreply.github.com>
2019-11-13 14:25:22 -0500

    Updates

---
+++ precache-manifest.b97a78f4745d87707dd5517c64f75469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f5b8216383d5e6e641f63d1ea48058",
+    "url": "/learnwithchris/basicapp/index.html"
+  },
+  {
+    "revision": "6d5c4fa12716f216120e",
+    "url": "/learnwithchris/basicapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cc0bca1ec38c2088fb2e",
+    "url": "/learnwithchris/basicapp/static/js/2.f6609821.chunk.js"
+  },
+  {
+    "revision": "6d5c4fa12716f216120e",
+    "url": "/learnwithchris/basicapp/static/js/main.043f1339.chunk.js"
+  },
+  {
+    "revision": "f559886deb5e5713d3c4",
+    "url": "/learnwithchris/basicapp/static/js/runtime-main.916b5474.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/learnwithchris/basicapp/static/media/logo.25bf045c.svg"
+  }
+]);
